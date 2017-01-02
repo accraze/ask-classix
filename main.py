a64de6e6d125434997a1866c47606f3f26d8fa75
accraze <accraze@gmail.com>
2017-01-02 14:45:36 -0800

    adding basic game methods

---
+++ main.py
@@ -0,0 +1,76 @@
+import logging
+
+from random import randint
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, statement, question, session
+
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+game_data = [
+    {'band': 'The Beatles', 
+        'members': ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr']},
+    {'band': 'Pink Floyd', 
+        'members': ['David Gilmour', 'Roger Waters', 'Richard Wright', 'Nick Mason']},
+    {'band': 'Led Zeppelin', 
+        'members': ['Robert Plant', 'Jimmy Page', 'John Paul Jones', 'John Bonham']},
+    {'band': 'Kiss', 
+        'members': ['Paul Stanley', 'Gene Simmons', 'Peter Criss', 'Ace Frehley']},
+    {'band': 'Cream', 
+        'members': ['Eric Clapton', 'Jack Bruce', 'Ginger Baker']},
+    {'band': 'Black Sabbath', 
+        'members': ['Ozzy Osbourne', 'Tommy Iommi', 'Bill Ward', 'Geezer Butler']},
+    {'band': 'Rush', 
+        'members': ['Ozzy Osbourne', 'Tommy Iommi', 'Bill Ward', 'Geezer Butler']},
+]
+
+
+@ask.launch
+
+def new_game():
+
+    welcome_msg = render_template('welcome')
+
+    return question(welcome_msg)
+
+
+@ask.intent("YesIntent")
+
+def next_round():
+
+    number = [randint(0, 3) for _ in range(3)]
+    record = game_date[number] 
+
+    round_msg = render_template('round', members=record['members'])
+
+    session.attributes['band'] = record['band']
+
+    return question(round_msg)
+
+
+@ask.intent("AnswerIntent", convert={'band': str})
+
+def answer(band):
+
+    answer = session.attributes['band']
+
+    if band == answer:
+
+        msg = render_template('win')
+
+    else:
+
+        msg = render_template('lose', answer=answer)
+
+    return statement(msg)
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
